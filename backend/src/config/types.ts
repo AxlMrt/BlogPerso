b218761e7777670d81119d6e8eb962dac4078354
Axel <martin.axel@sfr.fr>
2023-08-01 07:45:25 +0200

    change user type

--- backend/src/config/types.ts
+++ backend/src/config/types.ts
@@ -6,11 +6,31 @@ export interface IUser {
   firstName: string;
   lastName: string;
   password: string;
+  photo: string;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+  books: IBook[];
 }
 
 export interface IUserLogin {
   email: string;
   password: string;
+}
+
+export interface IBook {
+  id: string;
+  title: string;
+  author: string;
+  type: string;
+  year: number;
+  publisher: string;
+  feedBack: number;
+  isRead: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: string;
+  user: IUser;
 }
 
 export interface ITokenData {
