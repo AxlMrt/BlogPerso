438348758654604d174c79e5361d4de349bf5991
Axel <martin.axel@sfr.fr>
2023-08-08 11:55:16 +0200

    fix color on home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,11 +3,11 @@ import Table from '../components/table/Table';
 
 export default function HomePage() {
 	return (
-		<main>
+		<section className='bg-gray-50 dark:bg-gray-900'>
 			<div className=' w-5/6 m-auto mt-32'>
 				<Search />
 				<Table />
 			</div>
-		</main>
+		</section>
 	);
 }
