8cb71ab68e0bf1b8394b1820f6d47d433fad9ea3
Axel <martin.axel@sfr.fr>
2023-08-17 09:11:15 +0200

    add visibility to password

--- frontend/src/components/form/form_input/Input.tsx
+++ frontend/src/components/form/form_input/Input.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { BsEye, BsEyeSlash } from 'react-icons/bs'
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 interface Props {
 	type: 'email' | 'password' | 'text' | string;
@@ -20,6 +23,17 @@ export default function Input({
 	id,
 	isRequired,
 }: Props) {
+	const [passwordType, setPasswordType] = useState(type);
+
+
+	const togglePassword = () => {
+		if (passwordType === 'password') {
+			setPasswordType('text');
+			return;
+		}
+		setPasswordType('password');
+	};
+
 	return (
 		<div>
 			<label
@@ -28,14 +42,29 @@ export default function Input({
 			>
 				{labelText}
 			</label>
-			<input
-				type={type}
-				id={id}
-				className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-				placeholder={holder}
-				{...register(name)}
-				required={isRequired}
-			/>
+			<div className='relative'>
+				<input
+					type={passwordType}
+					id={id}
+					className='relative bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+					placeholder={holder}
+					{...register(name)}
+					required={isRequired}
+				/>
+				{type === 'password' && (
+					<button
+						type='button'
+						className="absolute top-0 bottom-0 right-3 text-gray-400"
+						onClick={togglePassword}
+					>
+						{passwordType === 'password' ? (
+							<BsEye size={20} />
+						) : (
+							<BsEyeSlash size={20} />
+						)}
+					</button>
+				)}
+			</div>
 		</div>
 	);
 }
