ed99518373a128cf670868f10867c9eba676e7ab
Axel <martin.axel@sfr.fr>
2023-08-18 16:04:08 +0200

    Try with gmail account

--- backend/src/config/secrets.ts
+++ backend/src/config/secrets.ts
@@ -5,12 +5,20 @@ let jwtSecret: string = process.env.JWT_SECRET as string;
 let jwtRefresh: string = process.env.JWT_REFRESH as string;
 let authSecret: string = process.env.AUTH_EMAIL as string;
 let passSecret: string = process.env.AUTH_PASSWORD as string;
+let clientId: string = process.env.OAUTH_CLIENTID as string;
+let clientSecret: string = process.env.OAUTH_CLIENT_SECRET as string; 
+let refreshToken: string = process.env.OAUTH_REFRESH_TOKEN as string;
+let accessToken: string = process.env.OAUTH_ACCESS_TOKEN as string;
+
 
 const secrets = {
   jwtSecret,
   jwtRefresh,
   authSecret,
   passSecret,
+  clientId,
+  clientSecret,
+  refreshToken,
   nodeEnv: process.env.NODE_ENV,
   port: Number(process.env.PORT),
   web: JSON.parse(process.env.WEBSITE_ACCESS as string)
--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -2,27 +2,51 @@ import nodemailer from 'nodemailer';
 import secrets from '../config/secrets';
 import HttpException from '../config/exceptions/HttpException';
 import { Options } from 'nodemailer/lib/mailer';
+import { google } from "googleapis";
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+const OAuth2 = google.auth.OAuth2;
 
-let transporter = nodemailer.createTransport({
-  host: "smtp.office365.com",
-  port: 587,
-  auth: {
-    user: secrets.authSecret,
-    pass: secrets.passSecret
-  }
-});
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error)
-  } else {
-    console.log(success);
-  }
-});
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    secrets.clientId,
+    secrets.clientSecret,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: secrets.refreshToken
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject();
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: secrets.authSecret,
+      accessToken,
+      clientId: secrets.clientId,
+      clientSecret: secrets.clientSecret,
+      refreshToken: secrets.refreshToken
+    }
+  } as SMTPTransport.Options);
+
+  return transporter;
+};
+
 
 const sendEmail = async (mailOptions: Options) => {
   try {
-    await transporter.sendMail(mailOptions);
+    let emailTransporter = await createTransporter();
+    await emailTransporter.sendMail(mailOptions);
     return;
   } catch (error) {
     throw new HttpException(500, 'Something went wrong');
