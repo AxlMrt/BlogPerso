0aa26ea77ba6a0200603cf7616ee4aba5ce445d3
Martin Axel <martin.axel@sfr.fr>
2023-09-07 16:54:35 +0200

    add capitalize

--- frontend/src/components/advert/Advert.tsx
+++ frontend/src/components/advert/Advert.tsx
@@ -7,7 +7,7 @@ export default function Advert() {
   const actualDate = new Date;
   const handleNav = (title: string) => {
     navigate(`/book/${title}`)
-  } 
+  }
 
   return (
     <div className="flex flex-col items-center ">
@@ -15,38 +15,38 @@ export default function Advert() {
         <h3 className="text-gray-700 text-2xl font-bold dark:text-white">
           Découvrez Votre Prochain Livre
         </h3>
-        <span className='text-sm tracking-wide dark:text-white'>BESTS SELLERS { actualDate.getFullYear() }</span>
+        <span className='text-sm tracking-wide dark:text-white'>BESTS SELLERS {actualDate.getFullYear()}</span>
       </div>
-    <div className="flex space-x-4 overflow-x-scroll justify-center items-center w-full" >
-      {
-        bestsSellers.map((book, index) => {
-          return (
-       <div
-        className="relative flex flex-col justify-between  bg-white bg-center shadow-md rounded-3xl  bg-contain bg-no-repeat text-gray-800  overflow-hidden cursor-pointer min-w-full w-64 h-64 my-2 md:min-w-0"
-        style={{ backgroundImage: `url('${book.photo}')`, }}
-        key={index}
-        onClick={() => handleNav(book.title)}
-       >
-        <div className="absolute bg-gray-800 opacity-50 inset-0 z-0"></div>
-        <div className="relative flex flex-row items-end  h-72 w-full ">
-          <div className="p-6 rounded-lg  flex flex-col w-full z-10 ">
-            <h4 className="mt-1 text-white text-xl font-semibold  leading-tight truncate">
-              {book.title}
-            </h4>
-            <div className="flex justify-between items-center ">
-              <div className="flex flex-col">
-                <h2 className="text-sm flex items-center text-gray-300 font-normal">
-                 <RiQuillPenFill />
-                 {book.author}
-                </h2>
+      <div className="flex space-x-4 overflow-x-scroll justify-center items-center w-full" >
+        {
+          bestsSellers.map((book, index) => {
+            return (
+              <div
+                className="relative flex flex-col justify-between  bg-white bg-center shadow-md rounded-3xl  bg-contain bg-no-repeat text-gray-800  overflow-hidden cursor-pointer min-w-full w-64 h-64 my-2 md:min-w-0"
+                style={{ backgroundImage: `url('${book.photo}')`, }}
+                key={index}
+                onClick={() => handleNav(book.title)}
+              >
+                <div className="absolute bg-gray-800 opacity-50 inset-0 z-0"></div>
+                <div className="relative flex flex-row items-end  h-72 w-full ">
+                  <div className="p-6 rounded-lg capitalize flex flex-col w-full z-10 ">
+                    <h4 className="mt-1 text-white text-xl font-semibold  leading-tight truncate">
+                      {book.title}
+                    </h4>
+                    <div className="flex justify-between items-center ">
+                      <div className="flex flex-col">
+                        <h2 className="text-sm flex items-center text-gray-300 font-normal">
+                          <RiQuillPenFill />
+                          {book.author}
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div> 
-          </div>
-        </div>
-      </div> 
-          )
-        })
-      }
+            )
+          })
+        }
       </div>
     </div>
   );
--- frontend/src/components/scrap_book/Leading.tsx
+++ frontend/src/components/scrap_book/Leading.tsx
@@ -8,13 +8,13 @@ interface Props {
 export default function Leading({ author, title }: Props) {
 
   return (
-    <div className=' text-center lg:text-left md:pb-6'>
-            <p className='text-sm text-gray-700 dark:text-gray-400'>{ author }</p>
-            <h3 className='text-2xl font-bold tracking-widest'> 
-              {
-                title
-              }
-            </h3>
+    <div className='capitalize text-center lg:text-left md:pb-6'>
+      <p className='text-sm text-gray-700 dark:text-gray-400'>{author}</p>
+      <h3 className='text-2xl font-bold tracking-widest'>
+        {
+          title
+        }
+      </h3>
     </div>
   )
 }
